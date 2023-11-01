fd79753fb26001086316505dcc289ce3109532df
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2023-11-01 14:39:46 +0700

    up

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <section className="text-light-1">
+      <p className="text-base-semibold">Log in with your Instagram account</p>
+      <div className="flex flex-col gap-y-3 w"></div>
+    </section>
+  );
+};
+
+export default Page;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import '../globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Next.js 13 ',
+  description: 'A Next.js 13 application',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="flex flex-row">
+          <section className="main-container">
+            <div className="w-full max-w-4xl">{children}</div>
+          </section>
+        </main>
+      </body>
+    </html>
+  );
+}
